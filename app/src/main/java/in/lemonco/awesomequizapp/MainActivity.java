de08ad22fb7476f9317e195fa0bcbd62899cd9d1
SanehYadav <saneh1988@gmail.com>
2016-07-01 01:37:52 +0530

    Added checkbox and radio button questions

--- app/src/main/java/in/lemonco/awesomequizapp/MainActivity.java
+++ app/src/main/java/in/lemonco/awesomequizapp/MainActivity.java
@@ -2,36 +2,36 @@ package in.lemonco.awesomequizapp;
 
 import android.content.Intent;
 import android.graphics.Color;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CheckBox;
 import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 import android.widget.TextView;
 import android.widget.Toast;
+import android.widget.ViewFlipper;
 
-import org.w3c.dom.Text;
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
     private int counter = 1;
     private int correct_answers_count = 0;
-    private TextView question;
     private TextView tracker;
+    private ViewFlipper VF;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        question = (TextView) findViewById(R.id.question_text);
-        tracker = (TextView) findViewById(R.id.tracker);
-        question.setText(Quiz.QUIZCARDS[counter - 1].getQuestion());
-        tracker.setText(counter + "/10");
-        EditText answer_text = (EditText) findViewById(R.id.answer_text);
-        answer_text.setTextColor(Color.BLUE);// have to set color programatically , dont know why color is notshowing when I set it via xml.
-
-
+        VF = (ViewFlipper) findViewById(R.id.viewFlipper);
+        displayQuestion(counter);
     }
 
     @Override
@@ -57,25 +57,71 @@ public class MainActivity extends AppCompatActivity {
     }
 
     //updates textview when next button is clicked. Also updates the counter
-    public void next(View v) {
-        EditText answer_text = (EditText) findViewById(R.id.answer_text);
-        String answer = answer_text.getText().toString();
-        if ((Quiz.QUIZCARDS[counter - 1].getAnswer()).equalsIgnoreCase(answer)) {
-            correct_answers_count++;
-        }
-        if (counter < 10) {
-            counter++;
-            question.setText(Quiz.QUIZCARDS[counter - 1].getQuestion());
-            tracker.setText(counter + "/10");
-            answer_text.setText("");
-        } else {
-            Button submit_button = (Button) findViewById(R.id.submit_button);
-            Button next_button = (Button) findViewById(R.id.next_button);
-            submit_button.setVisibility(View.VISIBLE);
-            next_button.setVisibility(View.INVISIBLE);
+    private void displayQuestion(int counter) {
+        if (counter < 11) {
+            VF.setDisplayedChild(0);
+            TextView question_singleChoice = (TextView) findViewById(R.id.question_text_singleChoice);
+            question_singleChoice.setText(Quiz.QUIZCARDS[counter - 1].getQuestion());
+            EditText answer_text = (EditText) findViewById(R.id.answer_text_singleChoice);
+            answer_text.setTextColor(Color.BLUE);
 
-            Toast.makeText(this, getString(R.string.completion_msg), Toast.LENGTH_SHORT).show();
+        } else if (counter == 11) {
+            VF.setDisplayedChild(1);
+            TextView question_text_multi = (TextView) findViewById(R.id.question_text_multiChoice);
+            question_text_multi.setText(Quiz.QUIZCARDS[counter - 1].getQuestion());
+
+        } else if (counter == 12) {
+            VF.setDisplayedChild(2);
+            TextView question_text_radio = (TextView) findViewById(R.id.question_text_singleChoiceRadio);
+            question_text_radio.setText(Quiz.QUIZCARDS[counter - 1].getQuestion());
         }
+        tracker = (TextView) findViewById(R.id.tracker);
+        tracker.setText(counter + "/12");
+    }
+
+    public void next(View v) {
+        if (VF.getDisplayedChild() == 0) {
+            EditText answer_text = (EditText) findViewById(R.id.answer_text_singleChoice);
+            String answer = answer_text.getText().toString();
+            if ((Quiz.QUIZCARDS[counter - 1].getAnswer()).equalsIgnoreCase(answer)) {
+                correct_answers_count++;
+            }
+            answer_text.setText("");  //Clear text on clicking next
+        } else if (VF.getDisplayedChild() == 1) {
+            CheckBox earth = (CheckBox) findViewById(R.id.earth);
+            CheckBox sun = (CheckBox) findViewById(R.id.sun);
+            CheckBox venus = (CheckBox) findViewById(R.id.venus);
+            CheckBox jupiter = (CheckBox) findViewById(R.id.jupiter);
+            ArrayList<String> answer = Quiz.QUIZCARDS[counter - 1].getMultiAnswer();
+            if (earth.isChecked() == answer.contains(earth.getText()) && sun.isChecked() == answer.contains(sun.getText()) && venus.isChecked() == answer.contains(venus.getText()) && jupiter.isChecked() == answer.contains(jupiter.getText())) {
+                correct_answers_count++;
+            }
+
+        } else if (VF.getDisplayedChild() == 2) {
+
+            RadioGroup radioGroup = (RadioGroup) findViewById(R.id.radioGroup);
+            int button = radioGroup.getCheckedRadioButtonId();
+            if(button!=-1) {
+                RadioButton radioButton = (RadioButton) radioGroup.findViewById(button);
+                String answer = (String) radioButton.getText();
+                if ((Quiz.QUIZCARDS[counter - 1].getAnswer()).equalsIgnoreCase(answer)) {
+                    correct_answers_count++;
+                }
+            }
+        }
+
+        if (counter < 12) {
+            counter++;
+            displayQuestion(counter);
+        } else {
+            Toast.makeText(this, getString(R.string.completion_msg), Toast.LENGTH_SHORT).show();
+            Button nextButton = (Button)findViewById(R.id.next_button);
+            Button submitButton = (Button)findViewById(R.id.submit_button);
+            nextButton.setVisibility(View.INVISIBLE);  //make next button invisible
+            submitButton.setVisibility(View.VISIBLE);   //make submit button visible
+
+        }
+
 
     }
 
--- app/src/main/java/in/lemonco/awesomequizapp/Quiz.java
+++ app/src/main/java/in/lemonco/awesomequizapp/Quiz.java
@@ -1,15 +1,32 @@
 package in.lemonco.awesomequizapp;
 
+import java.util.ArrayList;
+
 /**
  * Quiz class contains all the questions and answers in a Quiz array
  */
 public class Quiz {
-    private String question;
-    private String answer;
+    private String question, answer, answer1, answer2;
 
+    //constructor for Quiz with one correct answer
     public Quiz(String ques, String ans) {
         this.question = ques;
         this.answer = ans;
+    }
+
+    //constructor for Quiz with two correct answers
+    public Quiz(String ques, String ans, String ans1) {
+        this.question = ques;
+        this.answer = ans;
+        this.answer1 = ans1;
+    }
+
+    //constructor for quiz with three correct answers
+    public Quiz(String ques, String ans, String ans1, String ans2) {
+        this.question = ques;
+        this.answer = ans;
+        this.answer1 = ans1;
+        this.answer2 = ans2;
     }
 
     public final static Quiz[] QUIZCARDS = {
@@ -22,15 +39,26 @@ public class Quiz {
             new Quiz("What is the name of NASA’s most famous space telescope?", "hubble space telescope"),
             new Quiz("Ganymede is a moon of which planet?", "jupiter"),
             new Quiz("What is the name of Saturn’s largest moon?", "Titan"),
-            new Quiz("Olympus Mons is a large volcanic mountain on which planet?", "mars")
+            new Quiz("Olympus Mons is a large volcanic mountain on which planet?", "mars"),
+            new Quiz("Select all planets from the list?", "Earth", "Venus", "Jupiter"),
+            new Quiz("How many planets are in our solar system", "9")
     };
 
     public String getAnswer() {
         return this.answer;
-    }
+    } //getter method for single answer
 
     public String getQuestion() {
         return this.question;
+    } //getter method for question
+
+    //getter method for multiple answers
+    public ArrayList<String> getMultiAnswer() {
+        ArrayList<String> ans_arrayList = new ArrayList<String>();
+        ans_arrayList.add(answer);
+        ans_arrayList.add(answer1);
+        ans_arrayList.add(answer2);
+        return ans_arrayList;
     }
 }
 
